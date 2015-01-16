4f9fbfcd9488da466addb3abb0fb920eca3474da
Andres Torres <andrews_torres@hotmail.com>
2015-01-15 18:05:08 -0600

    Users fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true,
 	 					length: {maximum: 255}, format: {with: EMAIL_REGEX  },
 	 					uniqueness: {case_sensitive: false}
+	has_secure_password
+	validates :password, length: {minimum: 6}
 end
